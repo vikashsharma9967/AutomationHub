943cfbdb5269d49e0c0c9184148d5a179a789e51
Vikash Sharma <vikashsharma9967@gmail.com>
2024-09-29 20:30:27 +0530

    Third Commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -27,41 +27,42 @@ public class test {
         WebDriver driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+        System.out.println("Testing purpose");
         driver.get("https://automationexercise.com/");
         driver.findElement(By.xpath("//*[@href='/login']")).click();
         Thread.sleep(5000);
-        driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/input[2]")).sendKeys("vikash6726972");
-        driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/input[3]")).sendKeys("vikashhhsharm9696@gmail.com");
-        driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/button")).click();
-        Thread.sleep(10000);
-        driver.findElement(By.xpath("//*[@id=\"id_gender1\"]")).click();
-        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("Vikash@1998");
+//        driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/input[2]")).sendKeys("vikash6726972");
+//        driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/input[3]")).sendKeys("vikashhhsharm9696@gmail.com");
+//        driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/button")).click();
+//        Thread.sleep(10000);
+//        driver.findElement(By.xpath("//*[@id=\"id_gender1\"]")).click();
+//        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("Vikash@1998");
+////
+//        Select daydrop = new Select(driver.findElement(By.xpath("//*[@id=\"days\"]")));
+//        daydrop.selectByVisibleText("20");
 //
-        Select daydrop = new Select(driver.findElement(By.xpath("//*[@id=\"days\"]")));
-        daydrop.selectByVisibleText("20");
-
-        Select monthdrop = new Select(driver.findElement(By.xpath("//*[@id=\"months\"]")));
-        monthdrop.selectByVisibleText("March");
-
-        Select yeardrop = new Select(driver.findElement(By.xpath("//*[@id=\"years\"]")));
-        yeardrop.selectByVisibleText("1998");
-
-        driver.findElement(By.xpath("//*[@id=\"first_name\"]")).sendKeys("Vikash kumar");
-        driver.findElement(By.xpath("//*[@id=\"last_name\"]")).sendKeys("Sharma");
-        driver.findElement(By.xpath("//*[@id=\"address1\"]")).sendKeys("Katras bazar Dhanbad");
-
-        Select countrydrop = new Select(driver.findElement(By.xpath("//*[@id=\"country\"]")));
-        countrydrop.selectByVisibleText("India");
-
-        driver.findElement(By.xpath("//*[@id=\"state\"]")).sendKeys("Jharkhand");
-        driver.findElement(By.xpath("//*[@id=\"city\"]")).sendKeys("Dhanbad");
-        driver.findElement(By.xpath("//*[@id=\"zipcode\"]")).sendKeys("828114");
-        driver.findElement(By.xpath("//*[@id=\"mobile_number\"]")).sendKeys("7979948972");
-        driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div/div[1]/form/button")).click();
-        Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div/h2/b")).getText(),"ACCOUNT CREATED!");
-        driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div/div/a")).click();
-        Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a")).getText(),"Logout");
-        Thread.sleep(5000);
+//        Select monthdrop = new Select(driver.findElement(By.xpath("//*[@id=\"months\"]")));
+//        monthdrop.selectByVisibleText("March");
+//
+//        Select yeardrop = new Select(driver.findElement(By.xpath("//*[@id=\"years\"]")));
+//        yeardrop.selectByVisibleText("1998");
+//
+//        driver.findElement(By.xpath("//*[@id=\"first_name\"]")).sendKeys("Vikash kumar");
+//        driver.findElement(By.xpath("//*[@id=\"last_name\"]")).sendKeys("Sharma");
+//        driver.findElement(By.xpath("//*[@id=\"address1\"]")).sendKeys("Katras bazar Dhanbad");
+//
+//        Select countrydrop = new Select(driver.findElement(By.xpath("//*[@id=\"country\"]")));
+//        countrydrop.selectByVisibleText("India");
+//
+//        driver.findElement(By.xpath("//*[@id=\"state\"]")).sendKeys("Jharkhand");
+//        driver.findElement(By.xpath("//*[@id=\"city\"]")).sendKeys("Dhanbad");
+//        driver.findElement(By.xpath("//*[@id=\"zipcode\"]")).sendKeys("828114");
+//        driver.findElement(By.xpath("//*[@id=\"mobile_number\"]")).sendKeys("7979948972");
+//        driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div/div[1]/form/button")).click();
+//        Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div/h2/b")).getText(),"ACCOUNT CREATED!");
+//        driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div/div/a")).click();
+//        Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a")).getText(),"Logout");
+//        Thread.sleep(5000);
         //driver.quit();
 
     }
